f0e8086e06e3edce6bbd012a6210a04e25a92569
Jon Grythe Stødle <jonstodle@outlook.com>
2019-07-25 22:54:02 +0200

    Add test checking for NotFoundError

--- tests/get_progress_tests.rs
+++ tests/get_progress_tests.rs
@@ -6,6 +6,7 @@ use std::io;
 struct TestClient {
     pub upload_progress: usize,
     pub total_upload_size: usize,
+    pub status_code: usize,
 }
 
 impl HttpHandler for TestClient {
@@ -15,7 +16,7 @@ impl HttpHandler for TestClient {
         headers.insert("upload-offset".to_owned(), self.upload_progress.to_string());
 
         Ok(HttpResponse {
-            status_code: 204,
+            status_code: self.status_code,
             headers,
         })
     }
@@ -26,6 +27,7 @@ fn should_report_correct_upload_progress() {
     let client = tus_client::Client::new(TestClient{
         upload_progress: 1234,
         total_upload_size: 2345,
+        status_code: 204,
     });
 
     let progress = client.get_progress("/something")
@@ -33,4 +35,21 @@ fn should_report_correct_upload_progress() {
 
     assert_eq!(1234, progress.bytes_uploaded);
     assert_eq!(2345, progress.total_size.unwrap());
+}
+
+#[test]
+fn should_return_not_found_at_4xx_status() {
+    let client = tus_client::Client::new(TestClient{
+        upload_progress: 1234,
+        total_upload_size: 2345,
+        status_code: 400,
+    });
+
+    let result = client.get_progress("/something");
+
+    assert!(result.is_err());
+    match result {
+        Err(tus_client::Error::NotFoundError) => {},
+        _ => panic!("Expected 'Error::NotFoundError'")
+    }
 }
